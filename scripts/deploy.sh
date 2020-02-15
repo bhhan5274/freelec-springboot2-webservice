7fa400b72fc5c4356ccc5cb57dafe528a0725b6b
bhhan <hbh5274@gmail.com>
2020-02-15 21:33:39 +0900

    scripts/deploy.sh modified

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPOSITORY=/app/step2
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=freelec-springboot2-webservice
 
 echo "> Build 파일 복사"
@@ -28,6 +28,6 @@ echo "> ${JAR_NAME}에 실행권한 추가"
 chmod +x ${JAR_NAME}
 echo "> ${JAR_NAME 실행"
 
-nohup java -jar -Dspring.config.location=classpath:/application.properties,/app/application-oauth.properties,\
-/app/application-real-db.properties \
+nohup java -jar -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,\
+/home/ec2-user/app/application-real-db.properties \
 ${JAR_NAME} > $REPOSITORY/nohup.out 2>&1 &
